1fbcc9ad6261403a72475684ba228d93bfec8e56
Пользователь <Valenta2910@mail.ru>
2022-11-30 19:44:43 +0300

    в 4 задаче, сделал операцию умножения.

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,13 @@ public class Main {
         paper = paper - 7639;
         System.out.println(paper);
 
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
     }
 }
