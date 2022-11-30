5aeed63b184f6c5f7928ed9200454c24361d305a
Пользователь <Valenta2910@mail.ru>
2022-11-30 19:43:33 +0300

    в 3 задаче, сделал операцию вычитания.

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,13 @@ public class Main {
         paper = paper + 4;
         System.out.println(paper);
 
+        System.out.println("Задача 3");
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
     }
 }
